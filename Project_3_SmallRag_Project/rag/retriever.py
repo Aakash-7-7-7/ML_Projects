2450571dc96193d4cf0768dd76826dfc0c09b17e
Aakash-7-7-7 <niranjancharan75@gmail.com>
2026-02-19 21:56:49 +0530

    did moe change

--- Project_3_SmallRag_Project/rag/retriever.py
+++ Project_3_SmallRag_Project/rag/retriever.py
@@ -1,5 +1,7 @@
 def retrieve(vectorstore):
     return vectorstore.as_retriever(
-        search_type="similarity",
-        search_lwargs={"k":5}
+        search_type="similarity_score_threshold",
+        search_kwargs={
+            "score_threshold": 0.6
+        }
     )
